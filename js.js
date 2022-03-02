d8b70d2b616d2ec7886108142cf084fc2894029e
unknown <you@example.com>
2022-03-02 12:46:08 +0600

    BG & Focus

--- js.js
+++ js.js
@@ -1,16 +1,13 @@
 // Converting fahrenheit to celsius
-fahrenheit = 450
+// fahrenheit = 450
 // celsius = (fahrenheit - 32) * 5 / 9
 // console.log(fahrenheit + ' °F = ' + celsius.toFixed(2) + ' °C');
 
-
 apiKey = 'dcfb20b1418c65a124c79fabd0c38ee1'
-
 
 input = () => {
   cityInput = document.getElementById('cityInput')
   cityName = document.getElementById('cityName')
-  // cityName.innerText = cityInput.value
 
   url = `http://api.openweathermap.org/data/2.5/find?q=${cityInput.value}&appid=${apiKey}&units=metric`
 
@@ -23,11 +20,10 @@ input = () => {
       cloud = document.getElementById('cloud')
       cloud.innerText = data.list[0].weather[0].main
       cityInput.value = ''
-      iconId = `http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`
+      iconId = `http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@4x.png`
       icon = document.getElementById('icon')
       icon.setAttribute('src', iconId)
       document.getElementById('weather').style.display = 'block'
-
-      console.log(data.list);
     })
+  cityInput.focus()
 }
