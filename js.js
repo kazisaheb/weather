2e45681d3e739a3054a9bf592d31772eb82f5260
unknown <you@example.com>
2022-03-02 15:38:03 +0600

    http to https

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ apiKey = 'dcfb20b1418c65a124c79fabd0c38ee1'
 input = () => {
   cityInput = document.getElementById('cityInput')
   cityName = document.getElementById('cityName')
-  fetch(`http://api.openweathermap.org/data/2.5/find?q=${cityInput.value}&appid=${apiKey}&units=metric`)
+  fetch(`https://api.openweathermap.org/data/2.5/find?q=${cityInput.value}&appid=${apiKey}&units=metric`)
     .then(res => res.json())
     .then(data => {
       temper = document.getElementById('temper')
@@ -13,7 +13,7 @@ input = () => {
       temper.innerText = data.list[0].main.temp + ' °C'
       cloud = document.getElementById('cloud')
       cloud.innerText = data.list[0].weather[0].main
-      iconId = `http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@4x.png`
+      iconId = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@4x.png`
       icon = document.getElementById('icon')
       icon.setAttribute('src', iconId)
       // Dynamic favicon
