c837035009e810b9d94372b8f52094d8311e74f8
unknown <you@example.com>
2022-03-02 11:46:53 +0600

    first commit

---
+++ js.js
@@ -0,0 +1,30 @@
+// Converting fahrenheit to celsius
+fahrenheit = 450
+// celsius = (fahrenheit - 32) * 5 / 9
+// console.log(fahrenheit + ' °F = ' + celsius.toFixed(2) + ' °C');
+
+
+apiKey = 'dcfb20b1418c65a124c79fabd0c38ee1'
+
+
+input = () => {
+  cityInput = document.getElementById('cityInput')
+  cityName = document.getElementById('cityName')
+  // cityName.innerText = cityInput.value
+
+  url = `http://api.openweathermap.org/data/2.5/find?q=${cityInput.value}&appid=${apiKey}&units=metric`
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      temper = document.getElementById('temper')
+      cityName.innerText = data.list[0].name
+      temper.innerText = data.list[0].main.temp + ' °C'
+      cloud = document.getElementById('cloud')
+      cloud.innerText = data.list[0].weather[0].main
+      console.log(data.list);
+    })
+
+
+
+}
