04e46aa0d7df5b925c71879978c8fc2fc302bcba
unknown <you@example.com>
2022-03-02 14:59:23 +0600

    Code Cleanup

--- js.js
+++ js.js
@@ -1,37 +1,24 @@
-// Converting fahrenheit to celsius
-// fahrenheit = 450
-// celsius = (fahrenheit - 32) * 5 / 9
-// console.log(fahrenheit + ' °F = ' + celsius.toFixed(2) + ' °C');
-
 apiKey = 'dcfb20b1418c65a124c79fabd0c38ee1'
-
 input = () => {
   cityInput = document.getElementById('cityInput')
   cityName = document.getElementById('cityName')
-
   fetch(`http://api.openweathermap.org/data/2.5/find?q=${cityInput.value}&appid=${apiKey}&units=metric`)
     .then(res => res.json())
     .then(data => {
       temper = document.getElementById('temper')
       cityName.innerText = data.list[0].name
-
       // Dynamic title
       title = document.getElementById('title')
       title.innerText = 'Weather of ' + data.list[0].name
-
       temper.innerText = data.list[0].main.temp + ' °C'
       cloud = document.getElementById('cloud')
       cloud.innerText = data.list[0].weather[0].main
-
       iconId = `http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@4x.png`
-
       icon = document.getElementById('icon')
       icon.setAttribute('src', iconId)
-
       // Dynamic favicon
       favicon = document.getElementById('favicon')
       favicon.setAttribute('href', iconId)
-
       document.getElementById('weather').style.display = 'flex'
     })
   cityInput.value = ''
